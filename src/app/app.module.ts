69480478a91850e998093ecc309bdad8d3d50aec
Harris <harrispap03@gmail.com>
2020-09-27 16:01:22 +0300

    created the new user form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { HomeComponent } from './home/home.component';
 import { AddNewUserComponent } from './add-new-user/add-new-user.component';
 import { RecordComponent } from './record/record.component';
 import { QrScannerComponent } from './qr-scanner/qr-scanner.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -19,6 +22,10 @@ import { QrScannerComponent } from './qr-scanner/qr-scanner.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
