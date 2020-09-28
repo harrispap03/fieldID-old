755bc401ffdd39356ecc608bd0f95a6193cae0d2
Harris <harrispap03@gmail.com>
2020-09-28 17:01:51 +0300

    added QR scanner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,17 @@ import { HomeComponent } from './home/home.component';
 import { AddNewUserComponent } from './add-new-user/add-new-user.component';
 import { RecordComponent } from './record/record.component';
 import { QrScannerComponent } from './qr-scanner/qr-scanner.component';
+
+//Material Design stuff
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import {MatGridListModule} from '@angular/material/grid-list';
+
+// QR scanner module
+
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
+
 
 @NgModule({
   declarations: [
@@ -26,7 +33,8 @@ import {MatGridListModule} from '@angular/material/grid-list';
     BrowserAnimationsModule,
     MatButtonModule,
     MatInputModule,
-    MatGridListModule
+    MatGridListModule,
+    ZXingScannerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
