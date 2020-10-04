a70ebf1085577efe6129d640ce847084134aa5fd
Harris <harrispap03@gmail.com>
2020-10-04 09:51:18 +0300

    .

--- src/app/add-new-user/add-new-user.component.ts
+++ src/app/add-new-user/add-new-user.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable} from 'rxjs';
-import { tap,first } from 'rxjs/operators'
+import { tap,first } from 'rxjs/operators';
 
 interface User{
   name?: string;
@@ -15,12 +15,15 @@ interface User{
   templateUrl: './add-new-user.component.html',
   styleUrls: ['./add-new-user.component.scss']
 })
+
+
+
 export class AddNewUserComponent implements OnInit {
   myForm: FormGroup;
 
   loading = false;
   success = false;
-
+  
   constructor(private fb: FormBuilder, private afs: AngularFirestore) { }
 
   ngOnInit(): void {
@@ -31,7 +34,7 @@ export class AddNewUserComponent implements OnInit {
     })
 
   }
-
+  
   async submitHandler(){
     this.loading = true;
 
@@ -40,12 +43,16 @@ export class AddNewUserComponent implements OnInit {
     try {
       await this.afs.collection('users').add(formValue);
       this.success = true;
+      this.myForm.reset();
+      
     } catch(err){
       console.error(err)
     }
     
     this.loading = false;
+    
   }
 
 
 }
+
