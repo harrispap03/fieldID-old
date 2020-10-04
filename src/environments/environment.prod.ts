8ecbba5d8778315fc479c26537c233ab29af81de
Harris <harrispap03@gmail.com>
2020-10-04 09:16:36 +0300

    added the ability to add users to the database

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+  apiKey: "AIzaSyAhT6V1DrdSiD7ssnB9PE_by0RRSUPy4WU",
+  authDomain: "fieldid-907bc.firebaseapp.com",
+  databaseURL: "https://fieldid-907bc.firebaseio.com",
+  projectId: "fieldid-907bc",
+  storageBucket: "fieldid-907bc.appspot.com",
+  messagingSenderId: "364093492616",
+  appId: "1:364093492616:web:5eaffdb74d8c9c15ffbe3d",
+  measurementId: "G-MKBKD32KQ7"
+  }
 };
