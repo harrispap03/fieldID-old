3f58a92e32cbd18e3e8eb52ff5ddfb39261f5511
Harris <harrispap03@gmail.com>
2020-10-15 20:06:59 +0300

    cleaned up some code

--- src/app/qr-scanner/qr-scanner.component.ts
+++ src/app/qr-scanner/qr-scanner.component.ts
@@ -15,22 +15,7 @@ export class QrScannerComponent implements OnInit {
   ngOnInit(): void {
   }
   
-  ngAfterViewInit() {
-    this.delayAndTryHarder();
-   }
-
- async delayAndTryHarder() {
-     await this.delay(1000);
-     this.toggleTryHarder();
-  }
-
-  delay(ms: number) {
-    return new Promise( resolve => setTimeout(resolve, ms) );
-  }
-
-  toggleTryHarder(): void {
-    this.tryHarder = !this.tryHarder;
- }
+  
 
   onScanSuccess(result: string){
     this.result = result
