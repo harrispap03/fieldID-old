489aa572d68568ad85ce0da32a8815a208047dfc
harrispap03 <harrispap03@gmail.com>
2021-01-11 12:24:57 +0200

    .

--- src/app/qr-scanner/qr-scanner.component.ts
+++ src/app/qr-scanner/qr-scanner.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import 'rxjs/add/operator/toPromise';
 
 @Component({
   selector: 'app-qr-scanner',
@@ -9,14 +10,16 @@ import { AngularFirestore } from '@angular/fire/firestore';
 export class QrScannerComponent {
   error: string;
   userId: string;
-   data = {
-    firstName: 'Harry'
-  }
+  data = {}
+
   constructor(private afs: AngularFirestore) {}
 
   onScanSuccess(qrResult: string) {
     this.userId = qrResult;
-    this.afs.collection('recentUsers').doc(this.userId).set(this.data).then(value => console.log(value));
+    this.afs.collection('users').doc(this.userId).get().toPromise.then(snapshot =>{
+      this.data = snapshot.data();
+    });
+    this.afs.collection('recentUsers').doc(this.userId).set(this.data /* + the data that's on this document on the users collection */);
   }
 
   Error(e: Event) {
--- src/app/record/record.component.ts
+++ src/app/record/record.component.ts
@@ -1,8 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
-import { BehaviorSubject, Observable, Subject } from 'rxjs';
-import { scan, switchMap, map, filter } from 'rxjs/operators'
-//import { MatTableModule } from '@angular/material/table';
+import { Observable } from 'rxjs';
 
 export interface User {
   idNum: number;
@@ -13,70 +11,21 @@ export interface User {
 @Component({
   selector: 'app-record',
   templateUrl: './record.component.html',
-  styleUrls: ['./record.component.scss']
+  styleUrls: ['./record.component.scss'],
 })
+export class RecordComponent implements OnInit {
+  recentUserDoc;
+  user: Observable<any>;
 
-export class RecordComponent{
+  array = []
+  constructor(private afs: AngularFirestore){}
 
-}
-
-/*export class RecordComponent {
-
-
-  public readonly users$: Observable<User[]>; // Observable of type user array
-
-  public readonly user$: Observable<User> // Observable of type user
-
-  private readonly _userList: Subject<User>; // Subject _userList of type of user
-
-  private readonly userDoc: AngularFirestoreDocument<User>; // AFS document of type user
-
-  public userId: string // user's id as an observable
-
-  constructor(private db: AngularFirestore) {
-
-    this.userDoc = db.doc<User>(`users/${this.userId}`); // Given a firestore document id retrieve the document with this id and store it in userDoc
-    this.user$ = this.userDoc.valueChanges(); //
-
-    this._userList = new Subject<User>(); // Make a new subject called _userList of type user
-
-    this.users$ = this._userList.pipe(
-      scan((userList:User[], newUser: User): User[] => [...userList, newUser], []) // Adds a user to the user list
-    );
+  ngOnInit(){
+    this.recentUserDoc = this.afs.collection('recentUsers');
+    this.user = this.recentUserDoc.valueChanges();
   }
 
-  receiveUserId($qrResult){
-    this.userId = $qrResult
-  }
-
-  public addUser(u: User): void{
-    this._userList.next(u);
+  get userDoc(){
+    return this.recentUserDoc.ref.id;
   }
 }
-*/
-
-
-
-/* public readonly users$: Observable<User[]>;
-  public readonly user$: Observable<User>;
-
-  public _newUserId: BehaviorSubject<string> = new BehaviorSubject(null);
-  public _userList: User[] = [];
-
-  constructor(private db: AngularFirestore) {
-    this.users$ = this._newUserId.pipe(
-      filter((u) => !!u), // We don't want to work with null users
-      switchMap((userId) => {
-        return this.db.doc<User>(`users/${userId}`).valueChanges();
-      }),
-      map((user: User) => {
-        this._userList.push(user);
-        return this._userList;
-      })
-    );
-  }
-
-  receiveUserId(userId){
-    this._newUserId.next(userId);
-  }
-  */
